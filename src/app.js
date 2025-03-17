336038ca05400ac66eacd22e7a8b26408c0117b7
Dylane <tanolamene@gmail.com>
2025-03-17 14:01:43 -0300

    initialize Express application with MongoDB connection and basic routing

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./src/routes/index.js";
+
+const app = express();
+
+// middleware
+app.use(express.json()); 
+
+//loalhost:3000/
+app.use("/", routes);
+
+export default app;
