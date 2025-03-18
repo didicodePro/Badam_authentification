09526899e034beec0227c9910205583eb99d14da
Dylane <tanolamene@gmail.com>
2025-03-17 23:34:28 -0300

    implement user registration with password hashing and email normalization

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,12 +3,8 @@ import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
   try {
-    const { name, email, password } = req.body; //destructuring
-
-    const hashedPassword = await bcrypt.hash(password,10);
-
-    const user = await User.create({name, email, hashedPassword});
-    res.json(user);
+    const user = await User.create(req.body );
+    res.status(201).json(user);
   } catch (err) {
     res.json({ message: "Erreur serveur : ", error: err.message });
   }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = mongoose.Schema({
   name: { type: String },
@@ -6,5 +7,28 @@ const UserSchema = mongoose.Schema({
   password: { type: String },
 });
 
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+UserSchema.pre("save", function (next) {
+  this.email = this.email.toLowerCase(); // Normalise l'email
+  next();
+});
+
+UserSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate(); // Récupère les données à mettre à jour
+
+  if (update.password) {  // Si un mot de passe est envoyé dans la mise à jour
+    update.password = await bcrypt.hash(update.password, 10); // Hash le nouveau mot de passe
+  }
+
+  next(); // Passe à la suite
+});
+
+
 const User = mongoose.model("User", UserSchema);
-export default User;  
+export default User;
