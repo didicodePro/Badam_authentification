381ab909fb9ed19d5f5083091cde49d0ee5a18d6
Dylane <tanolamene@gmail.com>
2025-03-19 14:20:10 -0300

    add logging for query parameters in getUsers controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ export const getUsers = async (req,res) => {
   try {
     // const users = await User.find();
     // res.json(users);
+    console.log(req.query)
     res.json("Hello")
   } catch (error) {
     res.json({message: "Erreur serveur", error: error})
